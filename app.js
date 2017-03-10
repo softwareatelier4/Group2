7d231e4863272e27a6117d3c5cd3d8e7df1bc86f
Marco Tollini <mtollini@180dc.org>
2017-03-10 16:23:22 +0100

    Debugger + redis

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var app = express();
 var methodOverride = require('method-override')
 
 // Connect to MongoDB here
-var mongoose   = require('mongoose');
+var mongoose = require('mongoose');
 mongoose.connect(config.mongoUrl + config.mongoDbName);
 
 // Register model definition here
@@ -15,17 +15,19 @@ require('./models');
 
 //configure app
 app.use(logger('dev'));
-app.use(bodyParser.urlencoded({ extended: false }));    // parse application/x-www-form-urlencoded
-app.use(bodyParser.json());    // parse application/json
+app.use(bodyParser.urlencoded({
+   extended: false
+})); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride(
-function(req, res){
-  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-    var method = req.body._method
-    delete req.body._method
-    return method
-  }
-}
+   function(req, res) {
+      if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+         var method = req.body._method
+         delete req.body._method
+         return method
+      }
+   }
 ));
 
 // Initialize routers here
--- routes/api/root/router.js
+++ routes/api/root/router.js
@@ -3,20 +3,29 @@
 
 var express = require('express');
 var router = express.Router();
-var middleware =  require('../../middleware');
+var middleware = require('../../middleware');
 var rootUrl = require("../../../config").url;
-
+// redis
+const redis = require('redis');
+const REDIS_PORT = process.env.REDIS_PORT;
+const client = redis.createClient(REDIS_PORT);
 
 //supported methods
 router.all('/', middleware.supportedMethods('GET, OPTIONS'));
 
 //list users
 router.get('/', function(req, res, next) {
-  const test = {
-    ciao: 'test'
-  }
-    res.json(test);
+   const test = {
+      ciao: 'test'
+   }
+   client.set('some key', JSON.stringify({
+      ciao: 'come'
+   }));
 
+   client.get('some key', function(err, data) {
+      console.log(data);
+      res.json(JSON.parse(data));
+   })
 });
 /** router for /users */
 module.exports = router;
