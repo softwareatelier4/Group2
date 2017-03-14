21e8dedb501221be41b4e0f5f9fb91b53648c34d
Vanessa Braglia <braglv@usi.ch>
2017-03-14 15:03:29 +0100

    Search algorithm working!

--- public/js/script.js
+++ public/js/script.js
@@ -1,23 +1,23 @@
 //the function that is called when you search
 let search = function() {
-	animateSearch();
-	let query = document.getElementById('search-text').value;
-	console.log(query);
-	insertCard();
-	let spinner = document.getElementById('spinner');
-	spinner.style.display = "none";
+   animateSearch();
+   let query = document.getElementById('search-text').value;
+   console.log(query);
+   insertCard();
+   let spinner = document.getElementById('spinner');
+   spinner.style.display = "none";
 }
 
 //the animation that start when you search
 let animateSearch = function() {
-	let searchResult = document.getElementById('search-result');
-	searchResult.style.visibility = "visible";
-	searchResult.style.flexGrow = 1;
+   let searchResult = document.getElementById('search-result');
+   searchResult.style.visibility = "visible";
+   searchResult.style.flexGrow = 1;
 }
 
 // simulate a card insertion
 let insertCard = function() {
-	let card = `
+   let card = `
 	<div class='card result-card'>
 		<div class='card-img-top' alt='Card image cap'></div>
 		<div class='card-block'>
@@ -26,97 +26,6 @@ let insertCard = function() {
 			<a href='#' class='btn btn-primary'>Go somewhere</a>
 		</div>
 	</div>`;
-	let searchResult = document.getElementById('search-result');
-	searchResult.innerHTML+=card;
+   let searchResult = document.getElementById('search-result');
+   searchResult.innerHTML += card;
 }
-
-/**
- * Returns an array of Freelancers based on a given string
- * @param {array} freelancers - List of freelancer to filter
- * @param {string} string - Search criteria
- * @return {array} - Array of filtered freelancers
- */
-let searchEngine = function(freelancers, string) {
-	let result = [];
-	let words = string.replace(",", " ").split(" ");
-	let fClone = [];
-
-	for each (let w in words) {
-		for each (let f in fClone) {
-			if (searchForTag(f.tags, w).length > 0) {
-				fClone.push(f);
-				continue;
-			}
-
-			let city = [f.address.city];
-			if (searchForTag(city, w).length > 0) {
-				fClone.push(f);
-				continue;
-			}
-
-			let filter = [f.firstName, f.lastName, f.description, f.workName];
-			if (searchForTag(filter, w).length > 0) {
-				fClone.push(f);
-				continue;
-			}
-		}
-	}
-
-	for each (let f in fClone) {
-		let freelancer = {freelancer : f, counter : countInArray(fClone, f)};
-		result.push(freelancer);
-	}
-
-	result.sort(function(a, b){return b.counter-a.counter})
-
-	return result;
-
-}
-
-/**
- * Returns an array of Strings based on a given string and array of Strings
- * @param {array} array - List to iterate on (tags, cities, ...)
- * @param {string} string - Search criteria
- * @return {array} - Array of filtered stuff
- */
-let searchForTag = function(array, string) {
-	let result = [];
-	for each (let s in array) {
-		if (s.includes(string))
-			result.push(s);
-	}
-	return result;
-}
-
-/**
- * Returns the number of occurencies of an element in an array
- * @param {array} array - List to iterate on (tags, cities, ...)
- * @param {string} what - The element
- * @return {number} - Occurencies of that element in the array
- */
-function countInArray(array, what) {
-    var count = 0;
-    for (var i = 0; i < array.length; i++) {
-        if (array[i] === what) {
-            count++;
-        }
-    }
-    return count;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// ciao
--- routes/api/freelancer/router.js
+++ routes/api/freelancer/router.js
@@ -11,6 +11,14 @@ const Freelancer = mongoose.model('Freelancer');
 //supported methods
 router.all('/', middleware.supportedMethods('GET, OPTIONS'));
 
+router.get('/search/:search', function(req, res, next) {
+   Freelancer.find({}).populate('tags').lean().exec(function(err, freelancers) {
+      if (err) return next(err);
+
+      res.json(searchEngine(freelancers, req.params.search));
+   });
+});
+
 router.get('/:freelancerid', function(req, res, next) {
    Freelancer.findById(req.params.freelancerid).populate('tags').populate('ownerId').lean().exec(function(err, freelancer) {
       if (err) {
@@ -29,15 +37,110 @@ router.get('/:freelancerid', function(req, res, next) {
    })
 });
 
-router.get('/', function(req, res, next) {
-   Freelancer.find({}, fieldsFilter).lean().exec(function(err, freelancers){
-    if (err) return next (err);
-    freelancers.forEach(function(freelancer){
-      addLinks(freelancer);
-    });
-    res.json(freelancers);
-  });
-});
+/**
+ * Returns an array of Freelancers based on a given string
+ * @param {array} freelancers - List of freelancer to filter
+ * @param {string} string - Search criteria
+ * @return {array} - Array of filtered freelancers
+ */
+let searchEngine = function(freelancers, string) {
+   let result = [];
+   let words = string.replace(",", " ").split(" ");
+   let fClone = [];
 
-/** router for /users */
+   for (let w of words) {
+      for (let f of freelancers) {
+         let tags = [];
+         for (let t of f.tags) {
+            tags.push(t.name);
+         }
+         if (searchForTag(tags, w).length > 0) {
+            fClone.push(f);
+            continue;
+         }
+
+         let city = [f.address.city];
+         if (searchForTag(city, w).length > 0) {
+            fClone.push(f);
+            continue;
+         }
+
+         let filter = [f.firstName, f.lastName, f.description, f.workName];
+         if (searchForTag(filter, w).length > 0) {
+            fClone.push(f);
+            continue;
+         }
+      }
+   }
+
+   for (let f of fClone) {
+      let freelancer = {
+         _id: f._id,
+         firstName: f.firstName,
+         lastName: f.lastName,
+         description: f.description,
+         workName: f.workName,
+         photo: f.profilePhoto,
+         counter: countInArray(fClone, f)
+      };
+      result.push(freelancer);
+   }
+
+   result.sort(function(a, b) {
+      return b.counter - a.counter
+   })
+
+   return removeDuplicatesIds(result);
+
+}
+
+
+let removeDuplicatesIds = function(array) {
+   let temp = [];
+   let found = false;
+   for (let f of array) {
+      for (let x of temp) {
+         console.log(JSON.stringify(f));
+         if (f._id === x._id)
+            found = true;
+      }
+      if (!found)
+         temp.push(f);
+      found = false;
+   }
+   return temp;
+}
+
+/**
+ * Returns an array of Strings based on a given string and array of Strings
+ * @param {array} array - List to iterate on (tags, cities, ...)
+ * @param {string} string - Search criteria
+ * @return {array} - Array of filtered stuff
+ */
+let searchForTag = function(array, string) {
+   let result = [];
+   for (let s of array) {
+      if (s.includes(string))
+         result.push(s);
+   }
+   return result;
+}
+
+/**
+ * Returns the number of occurencies of an element in an array
+ * @param {array} array - List to iterate on (tags, cities, ...)
+ * @param {string} what - The element
+ * @return {number} - Occurencies of that element in the array
+ */
+function countInArray(array, what) {
+   var count = 0;
+   for (var i = 0; i < array.length; i++) {
+      if (array[i] === what) {
+         count++;
+      }
+   }
+   return count;
+}
+
+
 module.exports = router;
