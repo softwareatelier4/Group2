2f093adc2c78d0f686a8b0cc22afc2bc2759fb3a
Marco Tollini <mtollini@180dc.org>
2017-03-15 16:36:12 +0100

    Freelancer route test part 1

--- test/routes/api/freelancer.js
+++ test/routes/api/freelancer.js
@@ -4,9 +4,9 @@ var mongoose = require('mongoose');
 var ObjectId = mongoose.Types.ObjectId;
 
 var should = require('should');
-var utils = require('../utils');
-var app = require('../../app');
-var seedDb = require('../seedDb');
+var utils = require('../../utils');
+var app = require('../../../app');
+var seedDb = require('../../seedDb');
 var request = require('supertest');
 var freelancers;
 
@@ -17,25 +17,32 @@ describe('Testing Read for api/freelancer/', function() {
 
       it('Should return the correct freelancer', function(done) {
          request(app)
-            .get('api/freelancer/5625fc2bd82b84d23d8c7bd0/')
+            .get('/api/freelancer/5625fc2bd82b84d23d8c7bd0/')
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/, 'it should respond with json')
             .expect(200)
             .end(function(err, res) {
                var freelancer = res.text;
-               console.log('ciao', freelances);
-               // utils.matchAlbumInfoInText(res.text, album);
+               utils.matchFreelancerInfoInText(freelancer, freelancers[0]);
                done();
             });
       });
+
+      it('should respond with a 404 if the freelancer does not exist', function(done) {
+         request(app)
+            .get('/albums/' + ObjectId().toString())
+            .set('Accept', 'application/json')
+            .expect(404, done);
+      });
    });
 });
+
 
 function seed(done) {
    //seed the db
    seedDb.seed(function(err, seedData) {
       if (err) return done(err);
-      freelancers = seedData[1].data;
+      freelancers = seedData[0].data;
       done();
    });
 }
