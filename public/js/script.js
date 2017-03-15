fe4050ff681f94aa9a60614927e59a73ab53d1a9
LorenzoFerri <lorenzo.ferri@usi.ch>
2017-03-15 14:04:03 +0100

    dataVisualization setup

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,37 @@
+//setup before functions
+var typingTimer;                //timer identifier
+var doneTypingInterval = 500;  //time in ms, 5 second for example
+var $input = $('#search-text');
+
+//on keyup, start the countdown
+$input.on('keyup', function () {
+  clearTimeout(typingTimer);
+  typingTimer = setTimeout(search, doneTypingInterval);
+});
+
+//on keydown, clear the countdown
+$input.on('keydown', function () {
+  clearTimeout(typingTimer);
+});
+
 //the function that is called when you search
 let search = function() {
 	animateSearch();
-	let query = document.getElementById('search-text').value;
+	let query = $input[0].value; //$input is an array so the value is in the first element.
 	console.log(query);
-	insertCard();
-	let spinner = document.getElementById('spinner');
-	spinner.style.display = "none";
+	// insertCard();
+	doJSONRequest("GET","/api/freelancer/search?"+query,null,null,function(res) {
+		if(res.error){
+			console.log("error");
+		}
+		else{
+			for(freelancer of res){
+				insertCard(freelancer.name,freelancer.img,freelancer.description);
+			}
+		}
+		let spinner = document.getElementById('spinner');
+		spinner.style.display = "none";
+	});
 }
 
 //the animation that start when you search
@@ -16,13 +42,13 @@ let animateSearch = function() {
 }
 
 // simulate a card insertion
-let insertCard = function() {
+let insertCard = function(name,img,description) {
 	let card = `
 	<div class='card result-card'>
-		<div class='card-img-top' alt='Card image cap'></div>
+		<div class='card-img-top' alt='Card image cap' style="background-image: url("`+img+`")"></div>
 		<div class='card-block'>
-			<h4 class='card-title'>Card title</h4>
-			<p class='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+			<h4 class='card-title'>`+name+`</h4>
+			<p class='card-text'>`+description+`'s content.</p>
 			<a href='#' class='btn btn-primary'>Go somewhere</a>
 		</div>
 	</div>`;
