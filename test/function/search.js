7bcd4234ea5104bb0b6c727021a350b0bc35f482
Gargarensis <gabririchi@yahoo.it>
2017-03-15 14:28:21 +0100

    Beginning working on tests

--- test/function/search.js
+++ test/function/search.js
@@ -8,3 +8,50 @@ var utils = require('../utils');
 var app = require('../../app');
 var seedDb = require('../seedDb');
 var request = require('supertest');
+
+var freelancers;
+
+function seed(done){
+  //seed the db
+  seedDb.seed(function(err, seedData){
+    if (err) return done(err);
+    freelancers = seedData[1].data;
+    done();
+  });
+}
+
+
+describe('Testing the search algorithm', function(){
+
+  describe('GET /api/freelancer/search', function(){
+
+    before(seed);
+    after(utils.dropDb);
+
+    it('should list all the freelancer with correct data', function(done){
+      request(app)
+        .get('/api/freelancer/search/Lugano')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/, 'it should respond with json' )
+        .expect(200)
+        .end(function(err, res){
+          let resJson = res.json;
+          
+          done();
+        });
+    });
+  });
+});
+
+
+let matchFreelancerInfoInText = function matchAlbumInfoInText(text, album){
+   text.indexOf(album.name).should.be.greaterThan(-1, "name should be there");
+   text.indexOf(album.artist.toString()).should.be.greaterThan(-1, "name should be there");
+   text.indexOf(album.artwork).should.be.greaterThan(-1, "artwork should be there");
+   text.indexOf(album.label).should.be.greaterThan(-1, "label should be there");
+   if(! album.tracks) return;
+   album.tracks.forEach(function(track){
+     text.indexOf(track.toString())
+       .should.be.greaterThan(-1, "tracks should be there");
+   });
+}
