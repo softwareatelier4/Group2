801b81a370e4eae0517838a845a1456d17a83f1a
costanzavolpini <costanza.volpini@gmail.com>
2017-03-19 23:23:18 +0100

    Added template profile freelancer

--- public/js/freelancer.js
+++ public/js/freelancer.js
@@ -23,20 +23,38 @@ const FREELANCER = {
 		var idFreelancer = url.split('=')[1];
 
 		doJSONRequest("GET", "/api/freelancer/" + idFreelancer, null, null, function(res) {
-			console.log(res);
 			if (res.error) {
 				console.log("error");
 			} else {
 				$.get("/html/freelancer.html", function(html) {
-					html = html
-						.replace('{f.profilePic}', res.profilePhoto)
-						.replace('{f.firstName}', res.firstName)
-						.replace('{f.lastName}', res.lastName)
-						.replace('{f.city}', res.address.city)
-						.replace('{f.workName}', res.workName)
-						.replace('{f.mail}', res.email)
-						.replace('{f.phone}', res.phone)
-						.replace('{f.description}', res.description);
+					let templateTag = {
+						'f.city': res.address.city,
+						'f.profilePic': res.profilePhoto,
+						'f.firstName': res.firstName,
+						'f.lastName': res.lastName,
+						'f.workName': res.workName,
+						'f.mail': res.email,
+						'f.phone': res.phone,
+						'f.description': res.description
+					};
+
+					for (label in templateTag) {
+						let startTag = "{?" + label + "}";
+						let endTag = "{?" + label + "/}";
+						let startBloc = html.indexOf(startTag);
+						let endBloc = html.indexOf(endTag) + endTag.length;
+
+						if (templateTag[label] && startBloc != -1) {
+							html = html
+								.replace(startTag, '')
+								.replace(endTag, '')
+								.replace('{' + label + '}', templateTag[label]);
+						} else if (!templateTag[label] && startBloc != -1) {
+							html = html.substring(0, startBloc) + html.substring(endBloc, html.length);
+						} else {
+							html = html.replace('{' + label + '}', templateTag[label]);
+						}
+					}
 
 					MAIN_JS.innerHTML = html;
 				});
