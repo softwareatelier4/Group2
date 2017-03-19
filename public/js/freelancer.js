09a1791ee2004d9c18db29602a5e7e57505a1588
costanzavolpini <costanza.volpini@gmail.com>
2017-03-19 19:57:37 +0100

    Render Freelancer profile

--- public/js/freelancer.js
+++ public/js/freelancer.js
@@ -11,10 +11,36 @@ const FREELANCER = {
 		main.style.display = "inherit";
 		main.style.backgroundColor = "rgb(231, 231, 231)";
 
-		MAIN_QUERY.load('/html/freelancer.html');
+		FREELANCER.renderProfile();
 	},
 
 	remover: function() {
 
+	},
+
+	renderProfile: function() {
+		var url = window.location.href;
+		var idFreelancer = url.split('=')[1];
+
+		doJSONRequest("GET", "/api/freelancer/" + idFreelancer, null, null, function(res) {
+			console.log(res);
+			if (res.error) {
+				console.log("error");
+			} else {
+				$.get("/html/freelancer.html", function(html) {
+					html = html
+						.replace('{f.profilePic}', res.profilePhoto)
+						.replace('{f.firstName}', res.firstName)
+						.replace('{f.lastName}', res.lastName)
+						.replace('{f.city}', res.address.city)
+						.replace('{f.workName}', res.workName)
+						.replace('{f.mail}', res.email)
+						.replace('{f.phone}', res.phone)
+						.replace('{f.description}', res.description);
+
+					MAIN_JS.innerHTML = html;
+				});
+			}
+		});
 	}
 }
