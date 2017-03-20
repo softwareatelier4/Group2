c9c94215749bb67e0e34932747702ca2bee4cbe5
Marco Tollini <mtollini@180dc.org>
2017-03-21 00:32:35 +0100

    Seed update

--- seed.js
+++ seed.js
@@ -3,8 +3,8 @@
  */
 var seed = require('./test/seedDb').seed;
 
-seed(function(err, seedData2) {
-   if (err) console.log(err);
-   console.log("Seeding db complete!")
-   process.exit();
+seed(function(err, seedData) {
+	if (err) console.log(err);
+	console.log("Seeding db complete!")
+	process.exit();
 })
