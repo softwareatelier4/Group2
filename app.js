c419f3007931d8e4fea51d7bbdadc5cf975c9ffe
Marco Tollini <mtollini@180dc.org>
2017-03-20 19:14:30 +0100

    Frontend test working

--- app.js
+++ app.js
@@ -16,18 +16,18 @@ require('./models');
 //configure app
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({
-   extended: false
+	extended: false
 })); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride(
-   function(req, res) {
-      if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-         var method = req.body._method
-         delete req.body._method
-         return method
-      }
-   }
+	function(req, res) {
+		if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+			var method = req.body._method
+			delete req.body._method
+			return method
+		}
+	}
 ));
 
 // Initialize routers here
--- test/testUI/freelancer.js
+++ test/testUI/freelancer.js
@@ -7,11 +7,16 @@ var request = require('supertest');
 var Browser = require("zombie");
 var url = "http://localhost:3000/";
 
+var app = require('../../app');
 
 describe("testing freelancer frontend", function() {
 	var browser = new Browser();
 
 	before(function(done) {
+		// starting the server
+		app.set('port', process.env.PORT || 3000);
+		this.server = app.listen(app.get('port'));
+
 		browser.visit(url, done);
 	});
 
