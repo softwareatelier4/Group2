9614e69afe61cd5b240ce4b2d975467bd291626c
Marco Tollini <mtollini@180dc.org>
2017-03-21 00:15:22 +0100

    frontend test freelancer working

--- test/testUI/freelancer.js
+++ test/testUI/freelancer.js
@@ -1,72 +1,27 @@
-// 'use strict';
-//
-// var should = require('should');
-// var utils = require('../utils');
-// var request = require('supertest');
-// var app = require('../../app');
-//
-// var Browser = require("zombie");
-// Browser.localhost('127.0.0.1', 3000);
-//
-// describe("testing freelancer frontend", function() {
-// 	var browser = new Browser();
-//
-// 	before(function(done) {
-// 		// starting the server
-// 		// app.set('port', 3000);
-// 		// this.server = app.listen(app.get('port'));
-//
-// 		browser.visit('#', done);
-// 	});
-//
-// 	describe('Reach using search', function() {
-// 		it('should have the correct title', function() {
-// 			browser.assert.text('title', 'JobAdvisor');
-// 		});
-// 	});
-// });
-//
-// var should = require('should');
-// var utils = require('../utils');
-// var request = require('supertest');
-// var app = require('../../app');
-//
-// var Browser = require("zombie");
-// <<<<<<< Updated upstream
-// =======
-// <<<<<<< HEAD
-// Browser.localhost('127.0.0.1', 3000);
-//
-// describe("testing freelancer frontend", function() {
-// 	var browser = new Browser();
-//
-// 	before(function(done) {
-// 		// starting the server
-// 		// app.set('port', 3000);
-// 		// this.server = app.listen(app.get('port'));
-//
-// 		browser.visit('#', done);
-// =======
-// >>>>>>> Stashed changes
-//
-// describe("testing freelancer frontend", function() {
-// 	var browser = new Browser();
-//
-// 	before(function(done) {
-// 		// starting the server
-// 		app.set('port', 3000);
-// 		this.server = app.listen(app.get('port'));
-//
-// 		browser.visit('http://localhost:3000/', done);
-// <<<<<<< Updated upstream
-// =======
-// >>>>>>> origin/Init
-// >>>>>>> Stashed changes
-// 	});
-//
-// 	describe('Reach using search', function() {
-// 		it('should have the correct title', function() {
-// 			browser.assert.text('title', 'JobAdvisor');
-// 		});
-// 	});
-// });
+'use strict';
+
+var should = require('should');
+var utils = require('../utils');
+var request = require('supertest');
+var app = require('../../app');
+
+var Browser = require("zombie");
+Browser.localhost('127.0.0.1', 3000);
+
+describe("testing freelancer frontend", function() {
+	var browser = new Browser();
+
+	before(function(done) {
+		// starting the server
+		app.set('port', 3000);
+		this.server = app.listen(app.get('port'));
+
+		browser.visit('', done);
+	});
+
+	describe('Reach using search', function() {
+		it('should have the correct title', function() {
+			browser.assert.text('title', 'JobAdvisor');
+		});
+	});
+});
