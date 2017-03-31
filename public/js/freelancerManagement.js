ec3433845f40553e791c5af8d8d7ef23f82db9fe
Nevio Valsangiacomo <valsan@usi.ch>
2017-03-31 17:57:24 +0200

    Started doing form entries type check

--- public/js/freelancerManagement.js
+++ public/js/freelancerManagement.js
@@ -12,24 +12,24 @@ $('#modal-tags').selectize({
 
 const FREELANCERMANAGEMENT = {
 
-	name: 'FREELANCERMANAGEMENT',
+    name: 'FREELANCERMANAGEMENT',
 
-	getFreelancerInfo: function() {
+    getFreelancerInfo: function() {
         var url = window.location.href;
-		var idFreelancer = url.split('=')[1];
+        var idFreelancer = url.split('=')[1];
 
-		doJSONRequest("GET", "/api/freelancer/" + idFreelancer, null, null, function(res) {
-			if (res.error) {
-				console.log("error");
-			} else {
-				$.get("/html/freelancer.html", function(html) {
-					res.score = FREELANCER.getHtmlRankStar({
-						full: res.score,
-						empty: 5 - res.score
-					});
-					let data = {
-						freelancer: res
-					};
+        doJSONRequest("GET", "/api/freelancer/" + idFreelancer, null, null, function(res) {
+            if (res.error) {
+                console.log("error");
+            } else {
+                $.get("/html/freelancer.html", function(html) {
+                    res.score = FREELANCER.getHtmlRankStar({
+                        full: res.score,
+                        empty: 5 - res.score
+                    });
+                    let data = {
+                        freelancer: res
+                    };
 
                     console.log(data.freelancer);
                     $("#modal-firstName").val(data.freelancer.firstName);
@@ -43,61 +43,82 @@ const FREELANCERMANAGEMENT = {
                     $("#modal-number").val(data.freelancer.address.number);
                     $("#modal-description").val(data.freelancer.description);
 
-					// dust.renderSource(html, data, function(err, out) {
-					// 	MAIN_JS.innerHTML = out;
-					// 	FREELANCER.renderReview(idFreelancer);
-					// });
+                    // dust.renderSource(html, data, function(err, out) {
+                    //  MAIN_JS.innerHTML = out;
+                    //  FREELANCER.renderReview(idFreelancer);
+                    // });
                     
-				});
-			}
-		});
-	},
+                });
+            }
+        });
+    },
 
     checkData: function(){
-        document.getElementById("form-freelancer-management").submit();
+        // console.log(document.getElementById("modal-firstName").value);
+        var patt = /^([A-Za-z_ _-_è_ü_é_ö_à_ä_á_'])+$/;
+        let flag = true;
+        // console.log(patt.test(document.getElementById("modal-firstName").value));
+
+
+        if(!patt.test(document.getElementById("modal-firstName").value)){
+            flag = false;
+            document.getElementById("modal-firstName-label").className = 'error-red';
+            document.getElementById("modal-firstName-label").innerHTML = "firstname can only contain letters(a-z,A-Z) and spaces";
+        }
+
+        if(!patt.test(document.getElementById("modal-lastName").value)){
+            flag = false;
+            document.getElementById("modal-lastName-label").className = 'error-red';
+            document.getElementById("modal-lastName-label").innerHTML = "lastname can only contain letters(a-z,A-Z) and spaces";
+        }
+
+        if(flag){
+            document.getElementById("form-freelancer-management").submit();
+        }
+
     },
 
     submitFreelancer: function(){
         console.log("works");
         //PUT
     },
-	result : {},
-	addedTags : [],
+    result : {},
+    addedTags : [],
 
-	tagHinter: function() {
-		let tagText = document.getElementById('tags');
-		let tagsList = document.getElementById('tags-list');
-		let oldInput = tagText.value;
-		tagText.value = tagText.value.replace(/[^A-Za-z]/g, '');
-		if(tagText.value.length > 0 && oldInput == tagText.value && event.key != "Enter") {
-			var availableTags = [];
-			doJSONRequest("GET", "/api/tag/search/" + tagText.value, null, null, function(res) {
-				FREELANCERCREATION.result = res;
-				for(let tag of res) {
-					availableTags = availableTags.concat([tag.name]);
-				}
-				$( "#tags" ).autocomplete({
-					source: availableTags
-				});
-			});
-		}
-		if(tagText.value.length > 0 && event.key == "Enter" && !FREELANCERCREATION.addedTags.includes(tagText.value)){
-			doJSONRequest("POST", "/api/tag/", null, { 'name' : tagText.value }, function(res) {
-				let badge = `<span class="badge badge-primary">`+ res.name +`  <span style="cursor: pointer;" onclick="FREELANCERCREATION.removeTag(this)" data-id="`+ res._id +`" aria-hidden="true">&times;</span></span>  `;
-				FREELANCERCREATION.addedTags.push(res._id);
-				tagText.value = '';
-				tagsList.innerHTML += badge;
-			});
-		}
-	},
+    tagHinter: function() {
+        let tagText = document.getElementById('tags');
+        let tagsList = document.getElementById('tags-list');
+        let oldInput = tagText.value;
+        tagText.value = tagText.value.replace(/[^A-Za-z]/g, '');
+        if(tagText.value.length > 0 && oldInput == tagText.value && event.key != "Enter") {
+            var availableTags = [];
+            doJSONRequest("GET", "/api/tag/search/" + tagText.value, null, null, function(res) {
+                FREELANCERCREATION.result = res;
+                for(let tag of res) {
+                    availableTags = availableTags.concat([tag.name]);
+                }
+                $( "#tags" ).autocomplete({
+                    source: availableTags
+                });
+            });
+        }
+        if(tagText.value.length > 0 && event.key == "Enter" && !FREELANCERCREATION.addedTags.includes(tagText.value)){
+            doJSONRequest("POST", "/api/tag/", null, { 'name' : tagText.value }, function(res) {
+                let badge = `<span class="badge badge-primary">`+ res.name +`  <span style="cursor: pointer;" onclick="FREELANCERCREATION.removeTag(this)" data-id="`+ res._id +`" aria-hidden="true">&times;</span></span>  `;
+                FREELANCERCREATION.addedTags.push(res._id);
+                tagText.value = '';
+                tagsList.innerHTML += badge;
+            });
+        }
+    },
 
-	removeTag: function(span) {
-		let tagId = span.dataset.id;
-		let index = FREELANCERCREATION.addedTags.indexOf(tagId);
-		if(index > - 1) {
-			FREELANCERCREATION.addedTags.splice(index, 1);
-		}
-		span.parentNode.parentNode.removeChild(span.parentNode);
-	}
+    removeTag: function(span) {
+        let tagId = span.dataset.id;
+        let index = FREELANCERCREATION.addedTags.indexOf(tagId);
+        if(index > - 1) {
+            FREELANCERCREATION.addedTags.splice(index, 1);
+        }
+        span.parentNode.parentNode.removeChild(span.parentNode);
+    }
 
 }
