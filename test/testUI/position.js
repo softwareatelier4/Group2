3139fa5a7bfd78262dc1cf1fa4dfa063bac496cc
costanzavolpini <costanza.volpini@gmail.com>
2017-04-01 10:17:45 +0200

    testUI search(no-filter, one filter) and fixed test position

--- test/testUI/position.js
+++ test/testUI/position.js
@@ -1,18 +1,21 @@
 module.exports = {
-	'Check insertion of city-state search test': function(client) {
-		client
-			.url('http://localhost:3000/')
-			.waitForElementVisible('body', 1000)
-			.assert.visible('input[id=search-text]')
-			.assert.visible('span[id=basic-addon1]')
-			.clearValue('input[id=search-text]')
-			.setValue('input[id=position]', 'Lugano, Switzerland')
-			.pause(1000)
-			.click('span[id=basic-addon1]')
-			.pause(1000)
-			.assert.urlContains('city=Lugano')
-			.assert.urlContains('state=Switzerland')
-			.assert.urlContains('latitude=46.0134&longitude=8.9542')
-			.end();
-	}
+   'Check insertion of city-state search test': function(client) {
+      client
+         .url('http://localhost:3000/')
+         .waitForElementVisible('body', 1000)
+         .assert.visible('input[id=search-text]')
+         .assert.visible('span[id=basic-addon1]')
+         .pause(300)
+         .clearValue('input[id=search-text]')
+         .clearValue('input[id=position]')
+         .setValue('input[id=position]', 'Piacenza')
+         .pause(1000)
+         .setValue('input[id=position]', client.Keys.ENTER)
+         .click('span[id=basic-addon1]')
+         .pause(1000)
+         .assert.urlContains('city=Piacenza')
+         .assert.urlContains('state=Italy')
+         .assert.urlContains('latitude=45.0526206&longitude=9.692984499999966')
+         .end();
+   }
 };
--- test/testUI/search.js
+++ test/testUI/search.js
@@ -14,5 +14,36 @@ module.exports = {
          .assert.visible('div[id=f00000000000000000000000]')
          .assert.visible('div[id=f00000000000000000000006]')
          .end();
+   },
+   'Search filter test': function(client) {
+      client
+         .url('http://localhost:3000/')
+         .waitForElementVisible('body', 1000)
+         .assert.visible('input[id=search-text]')
+         .assert.visible('span[id=basic-addon1]')
+         .clearValue('input[id=search-text]')
+         .setValue('input[id=search-text]', 'Developer')
+         .click('span[id=basic-addon1]')
+         .pause(500)
+         .assert.urlContains('search=Developer')
+         .pause(1000)
+         .assert.visible('div[id=f00000000000000000000000]')
+         .assert.visible('div[id=f00000000000000000000006]')
+         .assert.visible('span[id=btn-distance]')
+         .click('span[id=btn-distance]')
+         .pause(500)
+         .assert.attributeContains('#btn-distance', 'data-sorttype', 'asc')
+         .pause(1000)
+         .getAttribute("#main-content .result-card:nth-child(1)", "id", function(result) {
+            this.assert.equal(result.value, 'f00000000000000000000000');
+         })
+         .getAttribute("#main-content .result-card:nth-child(2)", "id", function(result) {
+            this.assert.equal(result.value, 'f00000000000000000000006');
+         })
+         .clearValue('input[id=search-text]')
+         .end();
+   },
+   'Search more than one filter test': function(client) {
+
    }
 };
