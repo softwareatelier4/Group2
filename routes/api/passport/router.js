bcc0ee92c97cf291db26a0793c2582c8b97f3f91
Zeno Trevisan <zeno.trevisan@gmail.com>
2017-04-10 14:46:50 +0200

    Corrected router

--- routes/api/passport/router.js
+++ routes/api/passport/router.js
@@ -16,13 +16,17 @@ router.all('/', middleware.supportedMethods('GET, POST, PUT, OPTIONS'));
 
 passport.use('local-login', new LocalStrategy({
 	usernameField: "email",
-	passwordField: "password"
+	passwordField: "password",
 },
 function(email, password, done){
 	User.findOne({ email: email }, function (err, user) {
 		if (err) { return done(err); }
-		if (!user) { return done(null, false); }
-		if (!user.validPassword(password)) { return done(null, false); }
+		if (!user) { 
+			return done(null, false); 
+		}
+		if (!user.validPassword(password)) { 
+			return done(null, false);
+		}
 		return done(null, user);
 	});
 }
@@ -83,10 +87,15 @@ passport.deserializeUser(function (id,done) {
 	});
 });
 
-router.post('/login',
-passport.authenticate('local-login', { failureFlash: true }),
-function(req, res) {
-	res.send({result: "success"});
+router.post('/login',function(req, res, next) {
+  passport.authenticate('local-login', function(err, user, info) {
+    if (err) { return next(err); }
+    if (!user) { return res.send({result:'failed'}); }
+    req.logIn(user, function(err) {
+      if (err) { return next(err); }
+      return res.send({result:'success'});
+    });
+  })(req, res, next);
 });
 
 router.post('/signup', passport.authenticate('local-signup', {
