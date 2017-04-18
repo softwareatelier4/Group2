a75f4e776bb859b96c669883849c3878abb1eeb9
Zeno Trevisan <zeno.trevisan@gmail.com>
2017-04-18 13:05:35 +0200

    Added check for login
    Added a check to see if corrected login or not

--- public/js/login.js
+++ public/js/login.js
@@ -13,7 +13,11 @@ const LOGIN = {
 			'password' : password.value
         }
         doJSONRequest("POST", "/api/passport/login", null, login, function(res) {
-            console.log(res);
+            if(res.result == "success"){
+                window.location.replace("http://127.0.0.1:3000");
+            } else {
+                document.getElementById("modal-title").innerText = "User name or password wrong, try again!";
+            }
 		});
      }
 }
