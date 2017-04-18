1b3a44caba82787b2d94e04685d1e8cd0427aac9
Marco Tollini <mtollini@180dc.org>
2017-04-18 16:52:48 +0200

    added setBoxPosition helper to avoid false negative

--- test/testUI/position.js
+++ test/testUI/position.js
@@ -17,14 +17,13 @@ module.exports = {
 			.waitForElementVisible('body', 1000)
 			.assert.visible('input[id=search-text]')
 			.assert.visible('span[id=basic-addon1]')
-			.pause(1000)
+			.pause(100)
 			.clearValue('input[id=search-text]')
-			.clearValue('input[id=position]')
-			.setValue('input[id=position]', 'Piacenza')
-			.click('input[id=position]')
-			.waitForElementVisible('.pac-container', 60000)
-			.pause(500)
-			.setValue('input[id=position]', client.Keys.ENTER)
+
+		utils.setPositionBox(client, 'Piacenza');
+
+
+		client
 			.click('span[id=basic-addon1]')
 			.pause(1000)
 			.assert.urlContains('city=Piacenza')
