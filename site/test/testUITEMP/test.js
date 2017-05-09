18530e1735a2ac3526e38fdeb57f226b323fa1ea
Marco Tollini <mtollini@180dc.org>
2017-05-09 16:08:32 +0200

    again #2

--- site/test/testUITEMP/test.js
+++ site/test/testUITEMP/test.js
@@ -3,6 +3,9 @@
 module.exports = {
 	'Test modify freelancer': function(client) {
 		client
+			.resizeWindow(1500, 800)
+			.url('http://localhost:3000/#freelancer=f00000000000000000000002')
+			.waitForElementVisible('body', 1000)
 			.end();
 	}
 };
