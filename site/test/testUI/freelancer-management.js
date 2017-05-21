5b036e1f0508a415b8e8e91cf8c74587c7855d3c
Nevio Valsangiacomo <nevio.valsa@gmail.com>
2017-05-21 15:17:47 +0200

    Finished fronted tests

--- site/test/testUI/freelancer-management.js
+++ site/test/testUI/freelancer-management.js
@@ -14,7 +14,7 @@ module.exports = {
 	'Test modify freelancer': function(client) {
 		client
 			.resizeWindow(1500, 800)
-			.url('http://localhost:3000/#freelancer=f00000000000000000000000')
+			.url('http://127.0.0.1:3000/#freelancer=f00000000000000000000004')
 			.waitForElementVisible('body', 1000)
 
 			.pause(500)
@@ -24,7 +24,7 @@ module.exports = {
 			.click('#navbar-top-desktop a[name="login-link"]')
 			.pause(200)
 			.assert.visible('input#modal-password')
-			.setValue('input[id=login-email]', 'm.t@usi.ch')
+			.setValue('input[id=login-email]', 's.s@usi.ch')
 			.setValue('input#modal-password', 'test')
 			.pause(200)
 			.click('button[id=login-button]')
@@ -38,38 +38,27 @@ module.exports = {
 			.assert.visible('div[id=email]')
 			.assert.visible('div[id=phone]')
 
+
 			.click('button[id=modify-button]')
-			.pause(200)
-
-			.assert.visible('div[id=modal-ext]')
-
-			.assert.valueContains('input[id=modal-workName]', "Il Tollo")
-			.assert.valueContains('input[id=modal-phone]', "+39 380474747")
-
-
+			.pause(500)
 
 			.clearValue('input[id=modal-workName]')
 			.clearValue('input[id=modal-phone]')
+			.clearValue('textarea[id=modal-description]')
 
 			.setValue('input[id=modal-workName]', "Sample Name")
 			.setValue('input[id=modal-phone]', "+41 24 2222222")
+			.setValue('textarea[id=modal-description]', "New Description")
 
-			// .click('button[id=save-button]')
-			// .pause(1000)
-			//
-			// .assert.containsText('div[id=info-name-bottom]', "Sample Name")
-			// .assert.containsText('div[id=phone]', "+41 24 2222222")
-			//
-			// .click('button[id=modify-button]')
-			// .pause(200)
-			//
-			// .clearValue('input[id=modal-workName]')
-			// .clearValue('input[id=modal-phone]')
-			//
-			// .setValue('input[id=modal-workName]', "Il Tollo")
-			// .setValue('input[id=modal-phone]', "+39 380474747")
-			//
-			// .click('button[id=save-button]')
+			.pause(300)
+
+			.click('button[id=save-button]')
+			.pause(1000)
+			
+			.assert.containsText('div[id=info-name-bottom]', "Sample Name")
+			.assert.containsText('div[id=phone]', "+41 24 2222222")
+			.assert.containsText('span[id=description]', "New Description")
 			.end();
 	}
 };
+
--- site/test/testUITEMP/test.js
+++ site/test/testUITEMP/test.js
@@ -11,52 +11,53 @@ module.exports = {
 		utils.dropDbAndCloseConnection(done);
 	},
 
-	'Freelancer Creation': function(page) {
-		page
-			.url('http://localhost:3000/freelancerCreation.html')
+	'Test modify freelancer': function(client) {
+		client
+			.resizeWindow(1500, 800)
+			.url('http://127.0.0.1:3000/#freelancer=f00000000000000000000004')
 			.waitForElementVisible('body', 1000)
-			.assert.visible('div[id=freelancerCreation]')
-			.assert.visible('input[id=firstName-complex]')
-			.clearValue('input[id=firstName-complex]')
-			.setValue('input[id=firstName-complex]', 'Vincenzo')
-			.assert.visible('input[id=lastName]')
-			.clearValue('input[id=lastName]')
-			.setValue('input[id=lastName]', 'Ehiy')
-			.assert.visible('input[id=workName]')
-			.clearValue('input[id=workName]')
-			.setValue('input[id=workName]', 'Grunt')
-			.assert.visible('input[id=phone]')
-			.clearValue('input[id=phone]')
-			.setValue('input[id=phone]', '123123')
-			.assert.visible('input[id=position]')
-			.clearValue('input[id=position]')
-			.setValue('input[id=position]', 'Milano')
-			// .assert.visible('input[id=street]')
-			// .clearValue('input[id=street]')
-			// .setValue('input[id=street]', 'via Garibaldi')
-			// .assert.visible('input[id=number]')
-			// .clearValue('input[id=number]')
-			// .setValue('input[id=number]', '12')
-			// .assert.visible('input[id=zip]')
-			// .clearValue('input[id=zip]')
-			// .setValue('input[id=zip]', '20885')
-			.assert.visible('input[id=tags]')
-			.clearValue('input[id=tags]')
-			.setValue('input[id=tags]', 'Pescatore')
-			.sendKeys('input[id=tags]', page.Keys.ENTER)
-			.assert.visible('input[id=email]')
-			.clearValue('input[id=email]')
-			.setValue('input[id=email]', 'vincenzo.ehiy@virgilio.it')
-			.assert.visible('textarea[id=description]')
-			.clearValue('textarea[id=description]')
-			.setValue('textarea[id=description]', 'asd')
-			// .click('button[id=submit]')
-			// .pause(1000)
-			// .waitForElementVisible('main[id=main-content]', 1000)
-			// .waitForElementVisible('span[id=profile-name]', 1000)
-			// .assert.containsText('span[id=profile-name]', 'Vincenzo Ehiy')
-			// .assert.containsText('div[id=info-name-bottom]', 'Grunt')
-			// .assert.visible('div[id=tag]')
+
+			.pause(500)
+
+			.assert.elementPresent('a[name=login-link]')
+			.assert.visible('#navbar-top-desktop a[name="login-link"]')
+			.click('#navbar-top-desktop a[name="login-link"]')
+			.pause(200)
+			.assert.visible('input#modal-password')
+			.setValue('input[id=login-email]', 's.s@usi.ch')
+			.setValue('input#modal-password', 'test')
+			.pause(200)
+			.click('button[id=login-button]')
+			.pause(500)
+
+			.assert.visible('div[id=info-name-button]')
+			.pause(1000)
+			.assert.visible('button[id=modify-button]')
+			.assert.visible('div[id=info-name-bottom]')
+			.assert.visible('div[id=info]')
+			.assert.visible('div[id=email]')
+			.assert.visible('div[id=phone]')
+
+
+			.click('button[id=modify-button]')
+			.pause(500)
+
+			.clearValue('input[id=modal-workName]')
+			.clearValue('input[id=modal-phone]')
+			.clearValue('textarea[id=modal-description]')
+
+			.setValue('input[id=modal-workName]', "Sample Name")
+			.setValue('input[id=modal-phone]', "+41 24 2222222")
+			.setValue('textarea[id=modal-description]', "New Description")
+
+			.pause(300)
+
+			.click('button[id=save-button]')
+			.pause(1000)
+			
+			.assert.containsText('div[id=info-name-bottom]', "Sample Name")
+			.assert.containsText('div[id=phone]', "+41 24 2222222")
+			.assert.containsText('span[id=description]', "New Description")
 			.end();
 	}
 };
